2300937f107196773d95d10de89259b685244715
shadownetz <okwuzuchiagozie@gmail.com>
2021-04-01 21:32:33 +0100

    UI Updates

--- src/components/navigation/dashboard/topNav.vue
+++ src/components/navigation/dashboard/topNav.vue
@@ -212,7 +212,7 @@
                                         <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
                                         <span class="ml-2">Profile </span>
                                     </router-link>
-                                    <router-link :to="{name: 'Withdrawals'}" class="dropdown-item ai-icon">
+                                    <router-link :to="{name: 'Withdrawals'}" class="dropdown-item ai-icon" v-if="getUser.data.isAdmin">
                                         <img style="width: 20px;height:20px" src="../../../assets/images/svg/withdraw.png" alt="withdraw">
                                         <span class="ml-2">Withdrawal Requests </span>
                                     </router-link>
--- src/utils/mixins/basicDataMixins.js
+++ src/utils/mixins/basicDataMixins.js
@@ -1,7 +1,7 @@
 export default {
     data(){
         return {
-            whatsapp_contact_link: `https://api.whatsapp.com/send?phone=2349136318399&text=${encodeURI(
+            whatsapp_contact_link: `https://api.whatsapp.com/send?phone=2349138178720&text=${encodeURI(
                 "My name is ____. I need help with "
             )}`
         }
